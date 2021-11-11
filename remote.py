cea43e8e33d36feb9b2886265cf586c1ff26fff5
mephody-bro <kirill.fedyanin@gmail.com>
2021-11-11 13:28:17 +0300

    mnist rotated accuracy

---
+++ remote.py
@@ -0,0 +1,196 @@
+from nuq.nuq_classifier import NuqClassifier
+import sys
+import random
+
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib
+from torchvision.datasets import MNIST, CIFAR10, SVHN
+from torch.utils.data import DataLoader
+from torchvision.transforms import transforms
+import torch
+from torch import nn
+
+
+SEED = 1
+
+
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.cuda.manual_seed_all(SEED)
+
+train_transforms = transforms.Compose([
+    transforms.ToTensor()
+])
+test_transforms = transforms.Compose([
+    transforms.ToTensor()
+])
+
+mnist_train = MNIST('../checkpoint/data', download=True, train=True, transform=train_transforms)
+mnist_test = MNIST('../checkpoint/data', download=True, train=False, transform=test_transforms)
+
+
+train_loader = DataLoader(mnist_train, batch_size=512, shuffle=True)
+test_loader = DataLoader(mnist_test, batch_size=512)
+
+class SimpleConv(nn.Module):
+    def __init__(self):
+        super().__init__()
+        width = 32
+        self.layers = nn.Sequential(
+            nn.Conv2d(1, 16, 3, padding=1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(2),  # 14x14
+
+            nn.Conv2d(16, 32, 3, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(2), # 7x7
+
+            nn.Conv2d(32, 32, 3, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(),
+            nn.AvgPool2d(2, padding=1), # 4x4
+
+            nn.Flatten(),
+            nn.Linear(512, width, bias=False),
+            nn.BatchNorm1d(width),
+            nn.LeakyReLU(),
+
+        )
+
+        self.feature = None
+        self.linear =nn.Linear(width, 10)
+
+    def forward(self, x):
+        out = self.layers(x)
+        self.feature = out.clone().detach()
+        return self.linear(out)
+
+epochs = 1
+model = SimpleConv().cuda()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
+model.train()
+
+for e in range(epochs):
+    epoch_losses = []
+    for x_batch, y_batch in train_loader:
+        x_batch = x_batch.cuda()
+        y_batch = y_batch.cuda()
+        optimizer.zero_grad()
+        preds = model(x_batch)
+
+
+        loss = criterion(preds, y_batch)
+        loss.backward()
+        optimizer.step()
+        epoch_losses.append(loss.item())
+    print(np.mean(epoch_losses))
+
+model.eval()
+correct = []
+for x_batch, y_batch in test_loader:
+    with torch.no_grad():
+        x_batch = x_batch.cuda()
+        preds = torch.argmax(model(x_batch).cpu(), dim=-1)
+        correct.extend((preds == y_batch).tolist())
+print('Accuracy', np.mean(correct))
+
+
+
+rotation = (10, 20)
+corrupted_transforms = transforms.Compose([
+    transforms.RandomRotation(rotation),
+    transforms.ToTensor()
+])
+
+mnist_corrupted = MNIST('../checkpoint/data', download=True, train=False, transform=corrupted_transforms)
+corrupted_loader = DataLoader(mnist_corrupted, batch_size=10_000)
+images, labels = next(iter(corrupted_loader))
+
+
+with torch.no_grad():
+    preds = torch.argmax(model(images.cuda()), dim=-1).cpu()
+np.mean((preds == labels).numpy())
+ood_transforms = transforms.Compose([
+    transforms.Grayscale(),
+    transforms.Resize((28, 28)),
+    transforms.ToTensor()
+])
+
+# cifar = CIFAR10(
+#     '../checkpoint/data', download=True, train=False, transform=cifar_transforms
+# )
+ood_dataset = SVHN('./data', split='test', download=True, transform=ood_transforms)
+ood_loader = DataLoader(ood_dataset, batch_size=10_000)
+images_ood, labels_ood = next(iter(ood_loader))
+
+def entropy(x):
+    x_ = torch.softmax(x, dim=-1)
+    return torch.sum(-x_*torch.log(x_), dim=-1)
+
+with torch.no_grad():
+    preds = model(images.cuda()).cpu()
+    preds_ood = model(images_ood.cuda()).cpu()
+
+preds_train = None
+for x_batch, y_batch in train_loader:
+    with torch.no_grad():
+        preds_batch = model(x_batch.cuda()).cpu()
+    if preds_train is None:
+        preds_train = preds_batch
+        y_train = y_batch
+    else:
+        preds_train = torch.cat((preds_train, preds_batch), dim=0)
+        y_train = torch.cat((y_train, y_batch))
+
+preds_train = preds_train.numpy()
+y_train = y_train.numpy()
+
+
+ue_mnist = 1 - torch.max(torch.softmax(preds, dim=-1), dim=-1).values
+ue_ood = 1 - torch.max(torch.softmax(preds_ood, dim=-1), dim=-1).values
+ue = torch.cat((ue_mnist, ue_ood)).numpy()
+ue_entropy = torch.cat((entropy(preds), entropy(preds_ood)))
+
+
+
+nuq = NuqClassifier(tune_bandwidth='isj', n_neighbors=10)
+nuq.fit(X=preds_train, y=y_train)
+
+ue_nuq = np.concatenate((
+    nuq.predict_proba(preds.numpy(), return_uncertainty='epistemic')[1],
+    nuq.predict_proba(preds_ood.numpy(), return_uncertainty='epistemic')[1],
+))
+
+corrects = (torch.argmax(preds, dim=-1) == labels).numpy()
+
+xs = np.arange(0, 10001, 100)
+
+def splits(ues):
+    idxs = np.argsort(ues)
+    sorted_corrects = corrects[idxs]
+    ys = [1] + [np.mean(sorted_corrects[:num]) for num in xs[1:]]
+    return ys
+
+font = {'family' : 'normal',
+        # 'weight' : 'bold',
+        'weight': 'normal',
+        'size': 18}
+matplotlib.rc('font', **font)
+plt.figure(figsize=(6, 5), dpi=150)
+linewidth = 3.5
+plt.subplots_adjust(left=0.15, bottom=0.13, right=0.95)
+plt.title('Accuracy, MNIST rotated')
+plt.ylabel("Accuracy")
+plt.xlabel("Samples selected")
+plt.plot(xs, splits(ue_mnist), label='MaxProb', linestyle='--', color='tab:green', linewidth=linewidth)
+plt.plot(xs, splits(ue_entropy[:10000].numpy()), label='Entropy', linestyle=':', color='tab:red', linewidth=linewidth)
+# plt.plot(xs, splits(ues_test_ddu), label='DDU', linestyle='-', color='tab:cyan', linewidth=linewidth)
+plt.plot(xs, splits(ue_nuq[:10000]), label='NUQ', linestyle='-.', color='tab:purple', linewidth=linewidth)
+plt.legend()
+plt.show()
