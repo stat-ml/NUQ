89ca6ebb46be4f8cb9ff3a8b57ce86b2e9390a99
mephody-bro <kirill.fedyanin@gmail.com>
2021-11-11 16:33:03 +0300

    Add ddu/ensemble + spectral norm + aleatoric

--- remote.py
+++ remote.py
@@ -1,5 +1,4 @@
 from nuq.nuq_classifier import NuqClassifier
-import sys
 import random
 
 import numpy as np
@@ -10,11 +9,10 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
 import torch
 from torch import nn
+from torch.nn.utils import spectral_norm
 
 
 SEED = 1
-
-
 random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
@@ -35,74 +33,107 @@ mnist_test = MNIST('../checkpoint/data', download=True, train=False, transform=t
 train_loader = DataLoader(mnist_train, batch_size=512, shuffle=True)
 test_loader = DataLoader(mnist_test, batch_size=512)
 
+
+def entropy(x):
+    x_ = torch.softmax(x, dim=-1)
+    return torch.sum(-x_*torch.log(x_), dim=-1)
+
+
 class SimpleConv(nn.Module):
     def __init__(self):
         super().__init__()
         width = 32
         self.layers = nn.Sequential(
-            nn.Conv2d(1, 16, 3, padding=1, bias=False),
+            spectral_norm(nn.Conv2d(1, 16, 3, padding=1, bias=False)),
             nn.BatchNorm2d(16),
             nn.LeakyReLU(),
             nn.MaxPool2d(2),  # 14x14
 
-            nn.Conv2d(16, 32, 3, padding=1, bias=False),
+            spectral_norm(nn.Conv2d(16, 32, 3, padding=1, bias=False)),
             nn.BatchNorm2d(32),
             nn.LeakyReLU(),
             nn.MaxPool2d(2), # 7x7
 
-            nn.Conv2d(32, 32, 3, padding=1, bias=False),
+            spectral_norm(nn.Conv2d(32, 32, 3, padding=1, bias=False)),
             nn.BatchNorm2d(32),
             nn.LeakyReLU(),
             nn.AvgPool2d(2, padding=1), # 4x4
 
             nn.Flatten(),
-            nn.Linear(512, width, bias=False),
+            spectral_norm(nn.Linear(512, width, bias=False)),
             nn.BatchNorm1d(width),
             nn.LeakyReLU(),
-
         )
 
         self.feature = None
-        self.linear =nn.Linear(width, 10)
+        self.linear = nn.Linear(width, 10)
 
     def forward(self, x):
         out = self.layers(x)
         self.feature = out.clone().detach()
         return self.linear(out)
 
-epochs = 1
-model = SimpleConv().cuda()
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
-model.train()
 
-for e in range(epochs):
-    epoch_losses = []
-    for x_batch, y_batch in train_loader:
-        x_batch = x_batch.cuda()
-        y_batch = y_batch.cuda()
-        optimizer.zero_grad()
-        preds = model(x_batch)
+def get_model():
+    epochs = 5
+    model = SimpleConv().cuda()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
+    model.train()
 
-
-        loss = criterion(preds, y_batch)
-        loss.backward()
-        optimizer.step()
-        epoch_losses.append(loss.item())
-    print(np.mean(epoch_losses))
-
-model.eval()
-correct = []
-for x_batch, y_batch in test_loader:
-    with torch.no_grad():
-        x_batch = x_batch.cuda()
-        preds = torch.argmax(model(x_batch).cpu(), dim=-1)
-        correct.extend((preds == y_batch).tolist())
-print('Accuracy', np.mean(correct))
+    for e in range(epochs):
+        epoch_losses = []
+        for x_batch, y_batch in train_loader:
+            x_batch = x_batch.cuda()
+            y_batch = y_batch.cuda()
+            optimizer.zero_grad()
+            preds = model(x_batch)
 
 
+            loss = criterion(preds, y_batch)
+            loss.backward()
+            optimizer.step()
+            epoch_losses.append(loss.item())
+        print(np.mean(epoch_losses))
 
-rotation = (10, 20)
+    model.eval()
+    correct = []
+    for x_batch, y_batch in test_loader:
+        with torch.no_grad():
+            x_batch = x_batch.cuda()
+            preds = torch.argmax(model(x_batch).cpu(), dim=-1)
+            correct.extend((preds == y_batch).tolist())
+    print('Accuracy', np.mean(correct))
+    return model
+
+
+model = get_model()
+
+
+class Ensemble:
+    def __init__(self, models):
+        self.models = models
+
+    def __call__(self, x):
+        with torch.no_grad():
+            x_ = torch.mean(
+                torch.stack([m(x.cuda()).cpu() for m in self.models]),
+                dim=0
+            )
+        return x_
+
+    def ue(self, x):
+        with torch.no_grad():
+            x_ = torch.stack([m(x.cuda()).cpu() for m in self.models])
+            x_ = torch.mean(torch.softmax(x_, dim=-1), dim=0)
+        return torch.sum(-x_ * torch.log(x_), dim=-1)
+
+
+ensemble = Ensemble([get_model() for _ in range(5)])
+print(ensemble(next(iter(train_loader))[0]))
+
+
+rotation = (30, 45)
 corrupted_transforms = transforms.Compose([
     transforms.RandomRotation(rotation),
     transforms.ToTensor()
@@ -115,27 +146,9 @@ images, labels = next(iter(corrupted_loader))
 
 with torch.no_grad():
     preds = torch.argmax(model(images.cuda()), dim=-1).cpu()
-np.mean((preds == labels).numpy())
-ood_transforms = transforms.Compose([
-    transforms.Grayscale(),
-    transforms.Resize((28, 28)),
-    transforms.ToTensor()
-])
-
-# cifar = CIFAR10(
-#     '../checkpoint/data', download=True, train=False, transform=cifar_transforms
-# )
-ood_dataset = SVHN('./data', split='test', download=True, transform=ood_transforms)
-ood_loader = DataLoader(ood_dataset, batch_size=10_000)
-images_ood, labels_ood = next(iter(ood_loader))
-
-def entropy(x):
-    x_ = torch.softmax(x, dim=-1)
-    return torch.sum(-x_*torch.log(x_), dim=-1)
 
 with torch.no_grad():
     preds = model(images.cuda()).cpu()
-    preds_ood = model(images_ood.cuda()).cpu()
 
 preds_train = None
 for x_batch, y_batch in train_loader:
@@ -153,23 +166,52 @@ y_train = y_train.numpy()
 
 
 ue_mnist = 1 - torch.max(torch.softmax(preds, dim=-1), dim=-1).values
-ue_ood = 1 - torch.max(torch.softmax(preds_ood, dim=-1), dim=-1).values
-ue = torch.cat((ue_mnist, ue_ood)).numpy()
-ue_entropy = torch.cat((entropy(preds), entropy(preds_ood)))
+ue = ue_mnist
+ue_entropy = entropy(preds)
+ue_ensemble = ensemble.ue(images)
 
 
 
-nuq = NuqClassifier(tune_bandwidth='isj', n_neighbors=10)
+
+nuq = NuqClassifier(tune_bandwidth='classification', n_neighbors=100)
 nuq.fit(X=preds_train, y=y_train)
 
-ue_nuq = np.concatenate((
-    nuq.predict_proba(preds.numpy(), return_uncertainty='epistemic')[1],
-    nuq.predict_proba(preds_ood.numpy(), return_uncertainty='epistemic')[1],
-))
+ue_nuq = nuq.predict_proba(preds.numpy(), return_uncertainty='aleatoric')[1]
+
+
+train_loader = DataLoader(mnist_train, batch_size=60_000, shuffle=True)
+print(train_loader.batch_size)
+with torch.no_grad():
+    train_images, y_train = next(iter(train_loader))
+    y_train = y_train.numpy()
+    model(train_images.cuda())
+    train_embeddings = model.feature.cpu().numpy()
+
+    model(images.cuda()).cpu()
+    embeddings = model.feature.cpu().numpy()
+    print(embeddings[:5, :3])
+
+from spectral_normalized_models.ddu import (
+    gmm_fit, logsumexp
+)
+gaussians_model, jitter_eps = gmm_fit(
+    embeddings=torch.tensor(train_embeddings), labels=torch.tensor(y_train), num_classes=10
+)
+
+ues_test_ddu = gaussians_model.log_prob(torch.tensor(embeddings)[:, None, :].float())
+ues_test_ddu = -logsumexp(ues_test_ddu).numpy().flatten()
 
 corrects = (torch.argmax(preds, dim=-1) == labels).numpy()
 
-xs = np.arange(0, 10001, 100)
+xs = np.arange(0, 10001, 20)
+
+
+def show(idx):
+    print(torch.argmax(preds, dim=-1)[idx])
+    print(labels[idx])
+    plt.imshow(images[idx, 0], cmap='gray')
+    plt.show()
+
 
 def splits(ues):
     idxs = np.argsort(ues)
@@ -177,20 +219,23 @@ def splits(ues):
     ys = [1] + [np.mean(sorted_corrects[:num]) for num in xs[1:]]
     return ys
 
-font = {'family' : 'normal',
+font = { #'family' : 'normal',
         # 'weight' : 'bold',
         'weight': 'normal',
         'size': 18}
 matplotlib.rc('font', **font)
 plt.figure(figsize=(6, 5), dpi=150)
-linewidth = 3.5
+linewidth = 3
 plt.subplots_adjust(left=0.15, bottom=0.13, right=0.95)
-plt.title('Accuracy, MNIST rotated')
+plt.title('Accuracy, MNIST rotated, SN')
 plt.ylabel("Accuracy")
 plt.xlabel("Samples selected")
 plt.plot(xs, splits(ue_mnist), label='MaxProb', linestyle='--', color='tab:green', linewidth=linewidth)
-plt.plot(xs, splits(ue_entropy[:10000].numpy()), label='Entropy', linestyle=':', color='tab:red', linewidth=linewidth)
-# plt.plot(xs, splits(ues_test_ddu), label='DDU', linestyle='-', color='tab:cyan', linewidth=linewidth)
-plt.plot(xs, splits(ue_nuq[:10000]), label='NUQ', linestyle='-.', color='tab:purple', linewidth=linewidth)
+plt.plot(xs, splits(ue_entropy.numpy()), label='Entropy', linestyle=':', color='tab:red', linewidth=linewidth)
+plt.plot(xs, splits(ues_test_ddu), label='DDU', linestyle='-.', color='tab:cyan', linewidth=linewidth)
+plt.plot(xs, splits(ue_ensemble), label='Ensemble (m=5)', linestyle='-.', color='tab:orange', linewidth=linewidth)
+plt.plot(xs, splits(ue_nuq), label='NUQ', linestyle='-.', color='tab:purple', linewidth=linewidth)
 plt.legend()
 plt.show()
+
+import ipdb; ipdb.set_trace()
---
+++ spectral_normalized_models/ddu.py
@@ -0,0 +1,131 @@
+###
+# The implementation is strongly based on Jishnu Mukhoti (DDU author) own code
+# See https://github.com/omegafragger/DDU for the reference (MIT licencse)
+###
+import torch
+from torch import nn
+from tqdm import tqdm
+import torch.nn.functional as F
+
+DOUBLE_INFO = torch.finfo(torch.double)
+# JITTERS = [0, DOUBLE_INFO.tiny, 1e-32, 1e-24, 1e-20] + [10 ** exp for exp in range(-15, 0, 1)]
+JITTERS = [0, DOUBLE_INFO.tiny] + [10 ** exp for exp in range(-6, 0, 1)]
+
+
+
+def entropy(logits):
+    p = F.softmax(logits, dim=1)
+    logp = F.log_softmax(logits, dim=1)
+    plogp = p * logp
+    entropy = -torch.sum(plogp, dim=1)
+    return entropy
+
+
+def logsumexp(logits):
+    return torch.logsumexp(logits, dim=1, keepdim=False)
+
+
+def centered_cov_torch(x):
+    n = x.shape[0]
+    res = 1 / (n - 1) * x.t().mm(x)
+    return res
+
+
+def get_embeddings(
+    net, loader: torch.utils.data.DataLoader, num_dim: int, dtype, device, storage_device,
+):
+    num_samples = len(loader.dataset)
+    embeddings = torch.empty((num_samples, num_dim), dtype=dtype, device=storage_device)
+    labels = torch.empty(num_samples, dtype=torch.int, device=storage_device)
+
+    with torch.no_grad():
+        start = 0
+        for data, label in tqdm(loader):
+            data = data.to(device)
+            label = label.to(device)
+
+            if isinstance(net, nn.DataParallel):
+                out = net.module(data)
+                out = net.module.feature
+            else:
+                out = net(data)
+                out = net.feature
+
+            end = start + len(data)
+            embeddings[start:end].copy_(out, non_blocking=True)
+            labels[start:end].copy_(label, non_blocking=True)
+            start = end
+
+    return embeddings, labels
+
+
+def gmm_forward(net, gaussians_model, data_B_X):
+    if isinstance(net, nn.DataParallel):
+        features_B_Z = net.module(data_B_X)
+        features_B_Z = net.module.feature
+    else:
+        features_B_Z = net(data_B_X)
+        features_B_Z = net.feature
+
+    log_probs_B_Y = gaussians_model.log_prob(features_B_Z[:, None, :].float().cpu())
+
+    return log_probs_B_Y
+
+
+def gmm_evaluate(net, gaussians_model, loader, device, num_classes, storage_device):
+    num_samples = len(loader.dataset)
+    logits_N_C = torch.empty((num_samples, num_classes), dtype=torch.float, device=storage_device)
+    labels_N = torch.empty(num_samples, dtype=torch.int, device=storage_device)
+
+    with torch.no_grad():
+        start = 0
+        for data, label in tqdm(loader):
+            data = data.to(device)
+            label = label.to(device)
+
+            logit_B_C = gmm_forward(net, gaussians_model, data)
+
+            end = start + len(data)
+            logits_N_C[start:end].copy_(logit_B_C, non_blocking=True)
+            labels_N[start:end].copy_(label, non_blocking=True)
+            start = end
+
+    return logits_N_C, labels_N
+
+
+def gmm_get_logits(gmm, embeddings):
+
+    log_probs_B_Y = gmm.log_prob(embeddings[:, None, :])
+    return log_probs_B_Y
+
+
+def gmm_fit(embeddings, labels, num_classes):
+    gmm, jitter_eps = None, None
+    with torch.no_grad():
+        classwise_mean_features = torch.stack([torch.mean(embeddings[labels == c], dim=0) for c in range(num_classes)]).float()
+
+        width = embeddings.shape[-1]
+        classwise_cov_features = torch.zeros((num_classes, width, width), dtype=torch.float32)
+
+        for c in range(num_classes):
+            classwise_cov_features[c] = centered_cov_torch(embeddings[labels == c] - classwise_mean_features[c])
+
+    with torch.no_grad():
+        for jitter_eps in JITTERS:
+            # print('jitter eps', jitter_eps)
+            try:
+                jitter = jitter_eps * torch.eye(
+                    classwise_cov_features.shape[1], device=classwise_cov_features.device,
+                ).unsqueeze(0)
+                gmm = torch.distributions.MultivariateNormal(
+                    loc=classwise_mean_features, covariance_matrix=(classwise_cov_features + jitter),
+                )
+            except RuntimeError as e:
+                if "cholesky" in str(e):
+                    continue
+            except ValueError as e:
+                if "The parameter covariance_matrix has invalid values" in str(e):
+                    continue
+            break
+
+    return gmm, jitter_eps
